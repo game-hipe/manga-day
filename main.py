b5e1282ff12c5338871ae54be8138ade5d879394
GameHipe <alikegorplay@gmail.com>
2026-01-28 21:44:58 +0900

    added critical viewer

--- main.py
+++ main.py
@@ -35,5 +35,5 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         logger.info("Программа прервана пользователем.")
 
-    # except Exception as e:
-    #    logger.critical(f"Произошла ошибка: {e}")
+    except Exception as e:
+        logger.critical(f"Произошла ошибка: {e}")
