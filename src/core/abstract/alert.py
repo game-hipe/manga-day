98cd3030688e3e9f3749db60c3fe4598f2c205ce
GameHipe <alikegorplay@gmail.com>
2026-01-31 22:40:43 +0900

    added micro dockstring alert system

--- src/core/abstract/alert.py
+++ src/core/abstract/alert.py
@@ -2,5 +2,10 @@ from abc import ABC, abstractmethod
 
 
 class BaseAlert(ABC):
+    """
+    Абстрактный класс для отправки уведомлений.
+    """
+
     @abstractmethod
-    async def alert(self, message: str) -> None: ...
+    async def alert(self, message: str) -> None:
+        """Отправляет уведомление."""
